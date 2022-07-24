6d03c01f90e51a69feab478b387740a5f7d469fd
foysal <foysalf652@gmail.com>
2022-07-24 12:57:24 +0600

    fetchapi

--- API-Introductions/js/fetchapi.js
+++ API-Introductions/js/fetchapi.js
@@ -8,3 +8,34 @@ function info() {
         .then(json => console.log(json))
     
 }
+function findUser() {   
+    fetch('https://jsonplaceholder.typicode.com/comments')
+       .then(response => response.json())
+       .then(json => UserData(json))
+   
+}
+
+
+/* using for of collect all value of object // faster then (for and for in) loop */ 
+
+function UserData(json) {
+    for (const user of json) {
+        document.getElementById('status').innerHTML += `<li>${user.name}</li>` 
+    }  
+}
+
+/* using for in collect all index of array */
+
+// function UserData(json) {
+//     for (const user in json) {
+//         document.getElementById('status').innerHTML += `<li>${json[user].name}</li>` 
+//     }  
+// }
+
+/*  using forEach collect all user of Data*/
+
+// function UserData(json) {
+//     for(i = 0; i < json.length; i++) {
+//         document.getElementById('status').innerHTML += `<li>${json[i].name}</li>` 
+//     }
+// }
