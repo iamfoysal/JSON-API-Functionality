56fe477d81440cd2f81cda289755d569e92995be
foysal <foysalf652@gmail.com>
2022-07-27 11:53:35 +0600

    get data using python

--- index.py
+++ index.py
@@ -1,4 +1,43 @@
+from urllib.request import urlopen
+import requests
 import json
-def write_json(new_data, filename='first.json'):
-    with open  (filename, 'w') as f:
-        json.dump(new_data, f)
+
+
+# exempole 1 requete http
+
+
+# urls = "https://api.github.com/"
+
+# response = urlopen(urls)
+
+# data = json.load(response)
+
+# print(data)
+
+
+
+# exempole 2 requete http
+
+# url = 'https://randomfox.ca/floof/'
+# r = requests.get(url)
+
+# print(r.status_code) 
+
+# print(r.text) 
+# print(r.json()['image']) 
+
+# json_data=r.json()
+# print(json_data)
+
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+r = requests.get(url)
+
+# print(r.text) 
+json_data=r.json()
+print(json_data)
+print(json_data.title + " " + json_data.body)
+# print(r.json()['image']) 
+
+
